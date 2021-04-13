b0ab5dd31da269b5cf199e7fa2a5c0486edd9f7f
Basile Beyable <basile@beyable.com>
2021-04-13 10:34:57 +0200

    patch2 for deploy

--- fastcook_api/views.py
+++ fastcook_api/views.py
@@ -1,13 +1,13 @@
 import json
 from django.http import HttpResponse
 import requests
-from model import models
 
 
 def search_recipes(request):
     ingr = request.GET['ingr']
     url = "https://api.edamam.com/search?q={}&app_id=9b103c7a&app_key=d23af99b892e026f695ff215031e3ac7".format(
         ingr)
+
 
     response = requests.get(url).json()
 
@@ -24,6 +24,7 @@ def search_recipes(request):
         for ingredient in j:
             recipe["ingredients"].append(ingredient)
 
+        #recipeImporter(recipe)
         recipe_list.append(recipe)
 
     return HttpResponse(json.dumps(recipe_list, indent=4), content_type='application/json')
