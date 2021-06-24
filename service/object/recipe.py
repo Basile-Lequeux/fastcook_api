df3c3af6171dd9a67ae35fd9a173a3d0678487b4
Basile Beyable <basile@beyable.com>
2021-06-24 11:37:52 +0200

    Recipe url null=true

--- service/object/recipe.py
+++ service/object/recipe.py
@@ -6,8 +6,8 @@ Ingredient = ingredient.Ingredient
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    url = models.URLField()
-    imageUrl = models.URLField(null=True)
+    url = models.URLField(null=True, blank=True)
+    imageUrl = models.URLField(null=True, blank=True)
     ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
     totalTime = models.CharField(max_length=100, default="0.0")
     ingredientsDetail = models.JSONField(default=dict)
